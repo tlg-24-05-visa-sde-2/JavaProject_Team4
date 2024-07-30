f3d32a6697f9114fe9dee6a1e61a4111c4ca037a
Christopher_McIntosh <ChristopherM@tlgcohort.com>
2024-07-30 14:29:56 -0500

    Updated User Controller, User Repo, and User Service adding routes

--- src/main/java/com/gotakeahike/takeahike/repositories/UserRepository.java
+++ src/main/java/com/gotakeahike/takeahike/repositories/UserRepository.java
@@ -1,4 +1,19 @@
 package com.gotakeahike.takeahike.repositories;
 
-public class UserRepository {
+
+import com.gotakeahike.takeahike.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    User getById(Long id);
+
+    Boolean existsByUsername(String username);
+
 }
